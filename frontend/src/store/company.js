a84bfd385591d4dce75305f454e814a515ff7667
Muhammad Fauzan Rosyadi <fauzan.rosyadi@gmail.com>
2024-03-17 22:50:12 +0700

    remove dummy data

--- frontend/src/store/company.js
+++ frontend/src/store/company.js
@@ -3,72 +3,23 @@ import { createSlice } from '@reduxjs/toolkit';
 const companySlice = createSlice({
   name: 'company',
   initialState: {
-    companyList: [
-      {
-        "id": 1,
-        "name": "corteva1",
-        "address": "address1",
-        "revenue": "9898989",
-        "phoneCityCode": "98",
-        "phoneNumber": "7987987"
-      },
-      {
-        "id": 2,
-        "name": "corteva2",
-        "address": "address1",
-        "revenue": "9898989",
-        "phoneCityCode": "98",
-        "phoneNumber": "7987987"
-      },
-      {
-        "id": 3,
-        "name": "corteva3",
-        "address": "address1",
-        "revenue": "9898989",
-        "phoneCityCode": "98",
-        "phoneNumber": "7987987"
-      },
-      {
-        "id": 4,
-        "name": "corteva4",
-        "address": "address1",
-        "revenue": "9898989",
-        "phoneCityCode": "98",
-        "phoneNumber": "7987987"
-      },
-      {
-        "id": 5,
-        "name": "corteva5",
-        "address": "address1",
-        "revenue": "9898989",
-        "phoneCityCode": "98",
-        "phoneNumber": "7987987"
-      },
-      {
-        "id": 6,
-        "name": "corteva6",
-        "address": "address1",
-        "revenue": "9898989",
-        "phoneCityCode": "98",
-        "phoneNumber": "7987987"
-      },
-      {
-        "id": 7,
-        "name": "corteva7",
-        "address": "address1",
-        "revenue": "9898989",
-        "phoneCityCode": "98",
-        "phoneNumber": "7987987"
-      }
-    ]
+    companyList: []
   },
   reducers: {
     addCompany: (state, action) => {
       state.companyList = [ ...state.companyList, action.payload];
     },
+    deleteCompany: (state, action) => {
+      const idxToDelete = state.companyList.findIndex(c => c.id === action.payload)
+      if (idxToDelete !== -1) {
+        const newList = state.companyList.toSpliced(idxToDelete, 1)
+        console.log({idxToDelete, newList})
+        state.companyList = newList
+      }
+    }
   },
 });
 
-export const { addCompany} = companySlice.actions;
+export const { addCompany, deleteCompany} = companySlice.actions;
 
 export default companySlice.reducer;
